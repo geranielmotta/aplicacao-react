46e28aa22fcec3eceb04676be47793e4693093c7
Geraniel Motta <you@example.com>
2020-10-17 04:33:55 -0300

    ajuste de import

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import StatusBullet from './StatusBullet/StatusBullet'
 import RouteWithLayout from './RouteWithLayout/RouteWithLayout'
 import RouterSwitch from './RouterSwitch/RouterSwitch'
-import InputField from './InputField/InputField'
 import BreadcrumbsMain from './BreadcrumbsMain/BreadcrumbsMain'
 import Communication from './Communication/Communication'
 import Loader from './Loader/Loader'
@@ -11,7 +10,6 @@ export {
   StatusBullet,
   RouteWithLayout,
   RouterSwitch,
-  InputField,
   BreadcrumbsMain,
   Communication,
   Loader,
